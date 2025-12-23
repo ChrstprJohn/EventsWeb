36ec327ae59a10c01cc7a2b200d43e2e21faf50e
Christopher John <picardochristopherjohnoleo1@gmail.com>
2025-12-23 18:16:26 +0800

    implementing hog

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ const Page = () => {
    return (
       <section>
          <h1 className="text-center">
-            The hub for Every dev <br /> Event you cant miss
+            The Hub for Every Dev <br /> Event you Can't miss
          </h1>
          <p className="text-center mt-5">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -20,7 +20,7 @@ const Page = () => {
          <div className="mt-20 space-y-7">
             <h3>Featured Events</h3>
 
-            <ul className="events">
+            <ul className="events list-none m-0 p-0">
                {events.map((event) => (
                   <li key={event.title}>
                      <EventCard {...event} />
