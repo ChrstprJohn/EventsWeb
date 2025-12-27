df1fe6db898ab0ccdc963a8cd3439843bce573f2
Christopher John <picardochristopherjohnoleo1@gmail.com>
2025-12-28 05:58:09 +0800

    fix the error

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,23 @@
-import EventCard from '@/Components/EventCard';
 import ExploreBtn from '@/Components/ExploreBtn';
+import EventCard from '@/Components/EventCard';
 import { IEvent } from '@/database';
 import { cacheLife } from 'next/cache';
-// import { events } from "@/lib/constants";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const Page = async () => {
-    // throw new Error("Not Implemented");
     'use cache';
-    cacheLife('hours'); // delay created data for hours
-
+    cacheLife('hours');
     const response = await fetch(`${BASE_URL}/api/events`);
     const { events } = await response.json();
 
     return (
         <section>
             <h1 className='text-center'>
-                The Hub for Every Dev <br /> Event you Can't miss
+                The Hub for Every Dev <br /> Event You Can't Miss
             </h1>
             <p className='text-center mt-5'>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Hackathons, Meetups, and Conferences, All in One Place
             </p>
 
             <ExploreBtn />
@@ -28,11 +25,14 @@ const Page = async () => {
             <div className='mt-20 space-y-7'>
                 <h3>Featured Events</h3>
 
-                <ul className='events list-none m-0 p-0'>
+                <ul className='events'>
                     {events &&
                         events.length > 0 &&
                         events.map((event: IEvent) => (
-                            <li key={event.title}>
+                            <li
+                                key={event.title}
+                                className='list-none'
+                            >
                                 <EventCard {...event} />
                             </li>
                         ))}
